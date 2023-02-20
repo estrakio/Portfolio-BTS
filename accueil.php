9ff1abc51eb12e1caa515c29ea51a8835e91a37f
estrakio <walterkarl@hotmail.fr>
2023-02-20 23:24:14 +0100

    aa

--- accueil.php
+++ accueil.php
@@ -1,8 +1,28 @@
 <!-- Affichage du message de bienvenue ainsi que d'une image-->
-<div class="row text-center">
+<div class="row">
   <!-- A propos de moi Les bases-->
   <section>
-    
+    <div class="row bg-primary">
+      <div class="col-7 bg-warning" style="border-radius: 30px;">
+        <div class="row text-center">
+          <!-- La photo -->
+          <div class="col-5">
+            je suis la photo
+          </div>
+          <div class="col-7">
+            <h1> Walter Karl</h1>
+            <h2>25 juillet 1996</h2>
+            <h2>Technicien Développeur</h2>
+          </div>
+        </div>
+        <br>
+        <h2>BTS SIO option SLAM</h2>
+
+      </div>
+      <div class="col-5">
+
+      </div>
+    </div>
   </section>
 
   <!-- A propos de moi le texte -->
