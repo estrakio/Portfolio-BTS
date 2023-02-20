48d61c49b52fa0c14224f42d73a2ccdfd60e0aab
estrakio <walterkarl@hotmail.fr>
2023-02-20 18:18:16 +0100

    définiton des sections

--- accueil.php
+++ accueil.php
@@ -1,6 +1,27 @@
 <!-- Affichage du message de bienvenue ainsi que d'une image-->
-<div class="row">
-  <div class="text-center">
-    <b> Bienvenue sur Le portFolio de karl </b>
-  </div>
+<div class="row text-center">
+  <!-- A propos de moi Les bases-->
+  <section>
+    
+  </section>
+
+  <!-- A propos de moi le texte -->
+  <section>
+
+  </section>
+
+  <!-- Le BTS SIO -->
+  <section>
+
+  </section>
+
+  <!-- Travaux en Entreprise -->
+  <section>
+
+  </section>
+
+  <!-- Veille technologique -->
+  <section>
+
+  </section>
 </div>
