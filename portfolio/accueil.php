d3bb9fcc49e35074f704a3b4bf829fb34caed10f
estrakio <walterkarl@hotmail.fr>
2023-04-06 09:27:59 +0200

    modifs

--- portfolio/accueil.php
+++ portfolio/accueil.php
@@ -9,6 +9,7 @@
         <div class="row text-center">
           <!-- La photo -->
           <div class="col-5">
+            <!-- <img src="images/profil.png" width="300" height="300" class="img_rond" alt="Juste une photo de moi" /> -->
           </div>
           <div class="col-7" style="text-align: right;" >
             <h2> Walter Karl</h2>
@@ -31,18 +32,37 @@
       A propos de moi :
     </h2>
     <div class="row padding_content">
-      J'ai commencé mon parcours professionnel dans la restauration que j'ai poursuivit pendant 10 ans jusqu'a devenir
-      chef de cuisine.
-      <br>
-      J'ai entamé en 2018 mon projet de reconversion dans l'informatique par un stage dans la société Jus de Fruits
-      d'Alsace à Sarre-Union
-      ou j'ai pu découvrir l'aspect infrastructure ainsi que l'aspect développement des métiers de l'informatique.
-      <br>
-      Suite à ce stage j'ai commencé un an plus tard mon BTS SIO dans cette même société et en paralèle à l'école ORT à
-      Strasbourg.
-
-
+      <p>
+        J'ai commencé mon parcours professionnel dans la restauration que j'ai poursuivit pendant 10 ans jusqu'a devenir
+        chef de cuisine.
+        <br>
+        J'ai entamé en 2018 mon projet de reconversion dans l'informatique par un stage dans la société Jus de Fruits
+        d'Alsace à Sarre-Union
+        ou j'ai pu découvrir l'aspect infrastructure ainsi que l'aspect développement des métiers de l'informatique.
+        <br>
+        Suite à ce stage j'ai commencé un an plus tard mon BTS SIO dans cette même société et en paralèle à l'école ORT
+        à
+        Strasbourg.
+      </p>
     </div>
+
+    <h2 class="color_text_orange padding_title">
+      Mon projet professionnel :
+    </h2>
+    <div class="row padding_content">
+      <p>
+        Après mon BTS SIO, je préparerai au CESI durant 1 an le 
+        <br>
+        J'ai entamé en 2018 mon projet de reconversion dans l'informatique par un stage dans la société Jus de Fruits
+        d'Alsace à Sarre-Union
+        ou j'ai pu découvrir l'aspect infrastructure ainsi que l'aspect développement des métiers de l'informatique.
+        <br>
+        Suite à ce stage j'ai commencé un an plus tard mon BTS SIO dans cette même société et en paralèle à l'école ORT
+        à
+        Strasbourg.
+      </p>
+    </div>
+
     <h2 class="color_text_orange padding_title text-center">
       Langages appris durant ma formation
     </h2>
--- portfolio/miel.php
+++ portfolio/miel.php
@@ -6,20 +6,62 @@
   </div>
 
   <div class="row">
+    <!-- Partie gauche de la première section -->
     <div class="col-4 container">
+      <!-- Parti contenant Une grande image qui présente le projet et le logo vers le lien github en dessous -->
       <div class="row body_css">
         <img src="images/catastorphe.png" width="300" height="300" alt="Juste une photo de moi" />
       </div>
-      <div class="row text_couleur_bleu body_css ">
-        <!-- <div class="col-6"> -->
-          <img src="images/github.png" class="logo_git" alt="Juste une photo de moi" />
-        <!-- </div>
-        <div class="col-6"> -->
-          <a href="https://www.perdu.com">oui</a>
-        <!-- </div> -->
+      <div class="row text_couleur_bleu body_css text-center">
+        <!-- Afficher sur la même ligne le logo github et l'url -->
+        <a href="https://github.com/ratataque/Le_miel_et_les_abeilles" target="_blank">
+          <img src="images/github.png" width="80" height="80" alt="Logo Github" />
+        </a>
       </div>
     </div>
+    <!-- Partie droite de la première section -->
     <div class="col-8 container">
+      <!-- Parti contenant le titre du projet, la description et les technologies utilisées -->
+      <div class="row body_css ">
+        <h3 class="color_text_orange_clair ">Présentation : </h3>
+      </div>
+      <div class="row body_css">
+        <p class="text_couleur_bleu">
+          Projet Réalisé en cours de formation à l'école ORT. Le projet devait contenir deux Grande parti.
+        </p>
+        <div class="container">
+          <div class="row">
+            <div class="col-6">
+              <p class="color_text_orange">
+                <strong>SITE WEB :</strong>
+              </p>
+              <p class="text_couleur_bleu">
+                Création d'un site web pour une école qui propose une vente de miels pour financer un voyage scolaire.
+                <br>
+                Le site ce devait d'être simple et intuitif pour les utilisateurs car il représente la vitrine des produits mis en vente par l'école.
+                <br>
+                <br>
+                Il devait également comporter une partie administration pour permettre à l'école de gérer les produits vendu par les élèves et lui permetre de cloturer les ventes.
+
+              </p>
+            </div>
+            <div class="col-6">
+              <p class="color_text_orange">
+                <strong>APPLICATION MOBILE :</strong>
+              </p>
+              <p class="text_couleur_bleu">
+                Création d'un site web pour une entreprise de vente de miel.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row body_css">
+        <p class="color_text_orange_clair">
+          <strong>Technologies utilisées :</strong>
+        <p class="text_couleur_bleu">HTML, CSS, Bootstrap, PHP, Symfony, MySQL, Git, Github, Scrum</p>
+        </p>
+      </div>
     </div>
   </div>
 </div>
