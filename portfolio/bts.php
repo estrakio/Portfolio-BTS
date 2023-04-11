6858154ae9048267436dabe269abd76fb7250dab
estrakio <walterkarl@hotmail.fr>
2023-04-11 09:25:55 +0200

    .

---
+++ portfolio/bts.php
@@ -0,0 +1,29 @@
+<div class="container">
+  <h2 class="text-center color_text_orange">
+    Mon BTS SIO
+  </h2>
+  <div class="row">
+    <h4 class="color_text_orange padding_title">
+      L'école ORT à Strasbourg
+    </h4>
+
+    <div>
+      <!-- Créer une bannière avec une photo de l'école ORT -->
+    </div>
+    <div class="col-6 text_couleur_bleu">
+      <p>
+        fesfefsefs
+      </p>
+    </div>
+
+    <div class="col-6 text_couleur_bleu">
+      <p>
+        fqsfsfsef
+      </p>
+    </div>
+
+  </div>
+  <div>
+
+  </div>
+</div>
